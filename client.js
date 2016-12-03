ce3b2f34348af0c38d3c847348da7278453af326
Shawn Macburnie <shawnmacburnie@gmail.com>
2016-12-03 15:16:15 -0400

    removed not needed imports

--- client.js
+++ client.js
@@ -1,11 +1,6 @@
-var app = require('express')();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-var ioClient = require('socket.io-client');
 const spawn = require('child_process').spawn;
 var io = require("socket.io-client")('http://localhost:3000');
 let ping = null;
-var port = 3001;
 
 io.on("connect", function () {
     io.on('IP', function (data) {
